9ff874727f24fa6fa1403c64b93e2531624847f5
Alexey Berdnikov <alexey.berdnikov@poleinvest.ru>
2025-02-22 09:56:47 +0300

    Add media type defining to get

--- src/main/kotlin/ru/spb/sorface/controller/AvatarController.kt
+++ src/main/kotlin/ru/spb/sorface/controller/AvatarController.kt
@@ -2,7 +2,6 @@ package ru.spb.sorface.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import ru.spb.sorface.security.principal.SorfacePrincipal
 import ru.spb.sorface.service.AvatarService
+import ru.spb.sorface.utils.ImageUtils
 
 @RestController
 @RequestMapping("/internal/document/s3/avatar")
@@ -26,8 +26,8 @@ class AvatarController(
     }
 
     @GetMapping("/{userId}")
-    fun get(@PathVariable userId: String, @RequestParam(required = false) imageSize: String? = null) =
-        avatarService.get(userId, imageSize).let { ResponseEntity.ok().body(it) }
+    fun get(@PathVariable userId: String, @RequestParam(required = false) imageSize: String? = null) : ResponseEntity<ByteArray> =
+        avatarService.get(userId, imageSize).let { ResponseEntity.ok().contentType(ImageUtils.getImageType(it)).body(it) }
 
     @DeleteMapping("/{userId}")
     fun delete(@PathVariable userId: String): ResponseEntity<Nothing> = avatarService.delete(userId).let { ResponseEntity.noContent().build() }
--- src/main/kotlin/ru/spb/sorface/utils/ImageUtils.kt
+++ src/main/kotlin/ru/spb/sorface/utils/ImageUtils.kt
@@ -1,5 +1,8 @@
 package ru.spb.sorface.utils
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.server.ResponseStatusException
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
@@ -35,6 +38,30 @@ object ImageUtils {
         resizedImage.let { ImageIO.write(it, imageType, byteArrayOutputStream) }
 
         return byteArrayOutputStream.use { it.toByteArray() }
+    }
+
+    // Define the magic numbers for common image formats
+    private val JPEG_SIGNATURE = byteArrayOf(0xFF.toByte(), 0xD8.toByte(), 0xFF.toByte())
+    private val PNG_SIGNATURE = byteArrayOf(0x89.toByte(), 0x50.toByte(), 0x4E.toByte(), 0x47.toByte(), 0x0D.toByte(), 0x0A.toByte(), 0x1A.toByte(), 0x0A.toByte())
+    private val GIF_SIGNATURE = byteArrayOf(0x47.toByte(), 0x49.toByte(), 0x46.toByte())
+
+    /**
+     * Метод для получения типа изображения.
+     *
+     * @param imageData - изображение в виде массива байтов.
+     * @return - Медиа тип изображения
+     * @throws ResponseStatusException - если тип изображения не установлен
+     */
+    fun getImageType(imageData: ByteArray): MediaType {
+        return when {
+            imageData.copyOfRange(0, JPEG_SIGNATURE.size).contentEquals(JPEG_SIGNATURE) -> MediaType.IMAGE_JPEG
+
+            imageData.copyOfRange(0, PNG_SIGNATURE.size).contentEquals(PNG_SIGNATURE) -> MediaType.IMAGE_PNG
+
+            imageData.size >= 6 && imageData.copyOfRange(0, GIF_SIGNATURE.size).contentEquals(GIF_SIGNATURE) -> MediaType.IMAGE_GIF
+
+            else -> throw ResponseStatusException(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+        }
     }
 
 }
